fdd907b6c212f18d86910d39462eebd1795a1ca9
delnamerin2 <142641830+delnamerin2@users.noreply.github.com>
2023-09-20 21:21:52 +0530

    Delete javascrpit directory

--- javascrpit/q4/question4.js
+++
@@ -1,9 +0,0 @@
-function setOther(){
-    if(document.getElementById('food').value==='Other'){
-        document.getElementById("specify").style.display="block";
-    }
-    else{
-        document.getElementById("specify").style.display="none";
-    }
-    }
-
